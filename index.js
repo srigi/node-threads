e3bb8a60765e8533ec360e5f6c2538df23edb594
Igor Hlina <srigi@srigi.sk>
2015-12-12 08:32:57 +0100

    Modify entry script to use cluster

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 'use strict';
 
-require('babel-core/register');
-require('./app');
+var cluster = require('cluster');
+var chalk = require('chalk');
+var os = require('os');
+
+var CPUsNum = os.cpus().length;
+
+
+if (cluster.isMaster) {
+  console.log('Number of CPU cores', chalk.blue(CPUsNum));
+  console.log('starting', chalk.green('master'));
+  for (var i = 0; i < CPUsNum; i += 1) {
+    cluster.fork();
+  }
+
+} else {
+  console.log('starting', chalk.blue('child'));
+  require('babel-core/register');
+  require('./app');
+}
